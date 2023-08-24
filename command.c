a52430712ffc1697edc993866b23b483738c524a
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-08-24 15:03:50 -0700

    pratice

---
+++ command.c
@@ -0,0 +1,121 @@
+#include "monty.h"
+/**
+ * opcode_functions - operations to perform
+ * command: comand
+ * stack: stack
+ * line: line read
+ * Return: nothing
+ */
+void opcode_functions(char *command, stack_t **stack, unsigned int line)
+{
+	int j;
+
+	instruction_t operations[] = {{"pall", pall}, {"pint", pint},
+		{"pop", pop}, {"swap", swap}, {"add", add},
+		{"nop", nop}, {"sub", sub}, {"div", my_div}, {"mul", mul},
+		{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, {"rotl", rotl},
+		{NULL, NULL}};
+
+	for (j = 0; operations[j].opcode != NULL; j++)
+	{
+		if (strcmp(operations[j].opcode, command) == 0)
+		{
+			operations[j].f(stack, line);
+			return;
+		}
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line, command);
+	free_stack(stack);
+	exit(EXIT_FAILURE);
+}
+/**
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @line: the line number
+ *
+ * Return: Nothing.
+ */
+void pall(stack_t **stack, unsigned int line)
+{
+	stack_t *tmp;
+	(void)line;
+
+	tmp = *stack;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+/**
+ * _pint - prints the value at the top of the stack.
+ * @stack: Stack list
+ * @line_number: Number of the line
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+		fprintf(stderr, "%d\n", (*stack)->n);
+}
+/**
+ * pop - removes the top element of the stack.
+ * @stack: Stack list
+ * @line_number: Number of the line
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = NULL;
+
+	if (*stack == NULL || stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	current = *stack;
+
+	*stack = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+}
+/**
+ * swap - swaps the top two elements of the stack
+ *
+ * @head: head of the linked list
+ * @line: line number
+ * Return: no return
+ */
+void swap(stack_t **head, unsigned int line)
+{
+	int i = 0;
+	stack_t *tmp = NULL;
+
+	tmp = *head;
+
+	for (; tmp != NULL; tmp = tmp->next, i++)
+		;
+
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line);
+		free_stack(head);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *head;
+	*head = (*head)->next;
+	tmp->next = (*head)->next;
+	tmp->prev = *head;
+	(*head)->next = tmp;
+	(tmp)->prev = NULL;
+	tmp->next->prev = tmp;
+	tmp->prev = NULL;
+}
---
+++ command3.c
@@ -0,0 +1,101 @@
+#include "monty.h"
+/**
+ * mod - computes the rest of the division of the second top element
+ * @stack: Stack.
+ * @line_number: Number of line
+ * Return: nothing
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curerent = NULL;
+	int modd = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	curerent = *stack;
+	modd = curerent->next->n % curerent->n;
+	curerent->next->n = modd;
+	pop(stack, line_number);
+}
+/**
+ * pchar - prints the char at the top of the stack, followed by a new line.
+ * @stack: Stack.
+ * @line_number: Number of line
+ * Return: nothing
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int num = 0;
+
+	if (!*stack || !stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	num = (*stack)->n;
+	if (num < 0 || num > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	putchar(num);
+	putchar(10);
+}
+/**
+ * pstr - Print a string.
+ * @stack: stack
+ * @line_number: Number of line
+ * @Return: nothing
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+
+	while (current && current->n)
+	{
+		if (current->n < 32 || current->n > 127)
+			break;
+		printf("%c", current->n);
+		current = current->next;
+	}
+	putchar(10);
+}
+/**
+  *rotl - rotates the stack to the top
+  *@head: stack head
+  *@counter: line_number
+  *Return: nothing
+ */
+void rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
